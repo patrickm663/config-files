9138281dd9d0178a5a721790513208de7df30b7b
patrickm663 <rev0cation2025@outlook.com>
2023-05-01 18:44:44 +0200

    NVIM: add instructions to build from source

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -3,6 +3,7 @@
 
 # Install neovim from source
 sudo apt-get update
+sudo apt-get install ninja-build gettext cmake unzip curl -y
 cd ~/
 git clone https://github.com/neovim/neovim
 cd neovim/ && make CMAKE_BUILD_TYPE=RelWithDebInfo
