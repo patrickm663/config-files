6985662bf41b3831f5b9185047438636e9cfe62b
patrickm663 <rev0cation2025@outlook.com>
2023-05-01 18:38:06 +0200

    NVIM: add instructions to build from source

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,9 +1,12 @@
 #! bin/bash
 # Author: Patrick Moehrke
 
-# Install neovim
+# Install neovim from source
 sudo apt-get update
-sudo apt-get install neovim -y
+cd ~/
+git clone https://github.com/neovim/neovim
+cd neovim/ && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 
 # Copy nvim/ to ~/.config/
 mkdir -p ~/.config/nvim/
